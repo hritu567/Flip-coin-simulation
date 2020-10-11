8f71ab0a3bd9bd53c3651a7b9f320912e23bcec2
hritu567 <68611383+hritu567@users.noreply.github.com>
2020-10-11 20:47:35 +0530

    Update flipcoin.sh

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,8 +1,22 @@
 #!/bin/bash -x
-	randomCheck=$((RANDOM%2))
+read -p "Enter number of times coin should be tossed " num
+head=0
+tail=0	
+for (( i=0;i<=$num;i++))
+do
+randomCheck=$((RANDOM%2))
 if [ $randomCheck -eq 1 ]
 then
 	echo "Flipped Coin is Head"
+         ((head++))
 else
 	echo "Flipped Coin is Tail"
+         ((tail++))
+fi
+done
+if [ $head -gt $tail ] 
+then 
+     echo "head wins"
+else
+     echo "tail wins"
 fi
