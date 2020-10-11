c7fbc82ff83c34eeb56cb7596d35a29fb31e4875
hritu567 <hritumog96@gmail.com>
2020-10-11 10:16:55 +0530

    Added : head tail wining

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,2 +1,19 @@
 #!/bin/bash -x
-echo "Flip coin simulation"
+head=0
+tail=0
+var=$((RANDOM%2))
+while [ $head -lt 11 ] && [ $tail -lt 11 ]
+do
+if [ $var -eq 1 ]
+  then 
+     ((head++))
+else
+     ((tail++))
+fi
+done
+if [ $head -gt $tail ]
+  then
+    echo " head wins : " $head
+else
+    echo "tail wins : " $tail
+fi
