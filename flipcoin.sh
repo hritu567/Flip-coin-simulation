1d280c8897a267350faefdecb49eb3e356fb6ee7
hritu567 <hritumog96@gmail.com>
2020-10-11 20:17:41 +0530

    Added : winning difference b/w heads and tails

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,17 +1,26 @@
 #!/bin/bash -x
-read -p "Enter number of times to be tossed " num
-tails=0
-heads=0
-for ((counter=1; counter<=$num;counter++))
+head=0
+tail=0
+while [ $head -lt 21 ] && [ $tail -lt 21 ]
 do
-coin=$((RANDOM%2))
-if [ $coin -eq 0 ]
-then
-  ((tails++))
-else
-   ((heads++))
-fi 
-
+   ran=$((RANDOM%2))
+       if [ $ran -eq 1 ]
+       then
+           ((head++))
+       else
+           ((tail++))
+       fi
 done
-echo "Tails was tossed $tails times"
-echo "Heads was tossed $heads times"
+      if [ $head -eq 21 ]
+       then
+         diff=$(($head - $tail))
+        echo "heads win by $diff "
+      elif [ $tail -eq 21 ]
+       then
+         diff=$(($tail - $head))
+         echo "tails win by $diff "
+      else
+         echo "it is a tie "
+      fi
+echo "number of time head wins $head"
+echo "number of times tail wins $tail"
