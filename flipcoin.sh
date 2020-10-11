ab9a07d2132429acd2e4f83df77cc9b151d5c399
hritu567 <hritumog96@gmail.com>
2020-10-11 18:28:00 +0530

    Added : number of times tossed

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,18 +1,16 @@
 #!/bin/bash -x
-
-
-randomCheck=$((RANDOM%2))
-
-
-
-if [ $randomCheck -eq 1 ]
-
+read -p "Enter number of times to be tossed " num
+tails=0
+heads=0
+for ((counter=1; counter<=$num;counter++))
+do
+coin=$((RANDOM%2))
+if [ $coin -eq 0 ]
 then
-	
-  echo "Flipped Coin is Head"
-
+  ((tails++))
 else
-	
-   echo "Flipped Coin is Tail"
-
+   ((heads++))
 fi
+done
+echo "Tails was tossed $tails times"
+echo "Heads was tossed $heads times"
